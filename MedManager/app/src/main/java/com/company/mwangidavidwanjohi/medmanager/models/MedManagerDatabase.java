3987dd5f86fdfabe16fab7e90abcc33450e0d232
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-04-06 18:37:40 +0300

    Profile complated

---
+++ MedManager/app/src/main/java/com/company/mwangidavidwanjohi/medmanager/models/MedManagerDatabase.java
@@ -0,0 +1,12 @@
+package com.company.mwangidavidwanjohi.medmanager.models;
+import com.raizlabs.android.dbflow.annotation.Database;
+
+
+@Database(name = MedManagerDatabase.NAME, version = MedManagerDatabase.VERSION)
+
+public class MedManagerDatabase {
+    public static final String NAME = "MedManagerDatabase";
+
+    public static final int VERSION = 1;
+}
+
