6a7ddfdf8613f27d07be5f40e60a62621a279206
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-04-08 23:05:52 +0300

    preparing for alarm manager

--- MedManager/app/src/main/java/com/company/mwangidavidwanjohi/medmanager/adapters/ActiveMedicationAdapter.java
+++ MedManager/app/src/main/java/com/company/mwangidavidwanjohi/medmanager/adapters/ActiveMedicationAdapter.java
@@ -1,20 +1,32 @@
 package com.company.mwangidavidwanjohi.medmanager.adapters;
 
+import android.content.Context;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.company.mwangidavidwanjohi.medmanager.HomeActivity;
 import com.company.mwangidavidwanjohi.medmanager.R;
+import com.company.mwangidavidwanjohi.medmanager.fragments.ActiveMedicationFragment;
+import com.company.mwangidavidwanjohi.medmanager.fragments.CompletedMedicationFragment;
 import com.company.mwangidavidwanjohi.medmanager.models.Medication;
+import com.company.mwangidavidwanjohi.medmanager.models.Medication_Table;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
 
 import java.util.List;
 
 public class ActiveMedicationAdapter extends RecyclerView.Adapter<ActiveMedicationAdapter.ViewHolder>  {
         private List<Medication> myDataset;
-    public ActiveMedicationAdapter(List<Medication> dataset){
+         Context context;
+    public ActiveMedicationAdapter(Context c,List<Medication> dataset){
             myDataset=dataset;
+            this.context=c;
     }
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -26,28 +38,93 @@ public class ActiveMedicationAdapter extends RecyclerView.Adapter<ActiveMedicati
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
           //loop through the mydataset displaying each in a card view
-            Medication medication=myDataset.get(position);
+            final Medication medication=myDataset.get(position);
             holder.medicineName.setText(medication.name);
+            holder.description.setText(medication.description);
+            String frequncy=medication.amount_per_frequency+" X "+medication.frequency_in_a_day;
+            holder.frequency.setText(frequncy);
+            String medication_duration=medication.start_date+" To "+medication.end_date;
+            holder.duration.setText(medication_duration);
+            //determine if the alarm is activated or not
+            if (medication.activate_alarm){
+                holder.deactivate.setChecked(true);
+            }else{
+                holder.deactivate.setChecked(false);
+            }
+
+            //checkbox to deactivate a medication alarm
+        holder.deactivate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Medication new_value=myDataset.get(position);
+
+                if (holder.deactivate.isChecked()){
+                    holder.alarmStatus.setText("Alarm Enabled");
+                    Toast.makeText(context,"Alarm has been enabled for "+ medication.name,Toast.LENGTH_SHORT).show();
+                    //change the alarm value in the database to true
+                    new_value.activate_alarm=true;
+                    new_value.save();
+
+                }else {
+                    holder.alarmStatus.setText("Alarm Disabled");
+                    Toast.makeText(context,"Alarm has been disabled for "+medication.name,Toast.LENGTH_SHORT).show();
+                    //change the alarm value in the database to false
+                    new_value.activate_alarm=false;
+                    new_value.save();
+                }
+            }
+        });
+
+            //checkbox to mark medication as completed
+        holder.mark_completed.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Medication medi=myDataset.get(position);
+                //if checkbox is checked, change the value in db to completed
+                if (holder.mark_completed.isChecked()){
+                    Toast.makeText(context,"Compeleted",Toast.LENGTH_SHORT).show();
+                    medi.completed=true;
+                    medi.save();
+
+                    //reload the fragment to reflect the new records that are updated
+                    ActiveMedicationFragment activeMedicationFragment=new ActiveMedicationFragment();
+                    ((HomeActivity)context).getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,activeMedicationFragment).commit();
 
 
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return myDataset.size();
     }
-        public void updateData(List<Medication> updatedList){
+
+
+    public void updateData(List<Medication> updatedList){
+        myDataset.clear();
+
         myDataset=updatedList;
+
         this.notifyDataSetChanged();
-        }
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
-        TextView medicineName;
+        TextView medicineName,description,frequency,duration,alarmStatus;
+        CheckBox deactivate,mark_completed;
         public ViewHolder(View itemView) {
             super(itemView);
                 medicineName=(TextView)itemView.findViewById(R.id.active_medicine_name);
+                description=(TextView)itemView.findViewById(R.id.active_description);
+                frequency=(TextView)itemView.findViewById(R.id.frequency);
+                duration=(TextView)itemView.findViewById(R.id.active_start_end_date);
+                deactivate=(CheckBox) itemView.findViewById(R.id.deactivate_checkbox);
+                alarmStatus=(TextView)itemView.findViewById(R.id.alarm_status);
+                mark_completed=(CheckBox)itemView.findViewById(R.id.completed_checkbox);
+
         }
     }
 
